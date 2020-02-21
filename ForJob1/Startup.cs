af7466a77ea7b3fe1c8729b86ddcaf97d8738af1
khacnhat97 <42667036+khacnhat97@users.noreply.github.com>
2020-02-21 13:25:13 +0700

    Add project files.

---
+++ ForJob1/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ForJob1.Startup))]
+namespace ForJob1
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
